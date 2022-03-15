c9b2559fc3dad33c591743eda064052cc965c776
levintoo <tooklevin@gmail.com>
2022-03-15 17:33:25 +0300

    first commit

---
+++ resources/js/components/TodolistComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Todolist Component</div>
+                    <div class="card-body">
+                        I'm a todolist component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Todolist Component mounted.')
+        }
+    }
+</script>
